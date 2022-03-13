87a7e456df72a04a2ffcfe0016b06618b6cc1fae
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-03-13 01:02:27 +0000

    added FB popups I hope

--- frontend/utils/clientFB.js
+++ frontend/utils/clientFB.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase/app';
+
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN,
@@ -7,5 +9,10 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_APP_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID,
   };
-  
-export default firebaseConfig;
+
+export default function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    console.log('Firebase was successfully init.')
+  }
+}
