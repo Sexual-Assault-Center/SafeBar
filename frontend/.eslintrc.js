629f34908b4b54cbc18bef1e1c5575c6014fdbad
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-03-13 01:31:33 +0000

    testing

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  extends: ["next", "next/core-web-vitals", "prettier", "airbnb", "plugin:storybook/recommended"],
+  extends: ['next', 'next/core-web-vitals', 'prettier', 'airbnb', 'plugin:storybook/recommended'],
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['prettier'],
   rules: {
@@ -18,28 +18,28 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'no-alert': 'off',
     'react/jsx-filename-extension': [1, {
-      extensions: ['.js', '.jsx']
+      extensions: ['.js', '.jsx'],
     }],
     'react/jsx-one-expression-per-line': [0],
     'no-console': [1, {
-      allow: ['error', 'warn']
+      allow: ['error', 'warn'],
     }],
     'comma-dangle': ['error'],
     'no-debugger': 1,
     'linebreak-style': 0,
     'max-len': [1, 600, 2],
     'no-plusplus': [2, {
-      allowForLoopAfterthoughts: true
+      allowForLoopAfterthoughts: true,
     }],
     'jsx-a11y/label-has-associated-control': ['error', {
       required: {
-        some: ['nesting', 'id']
-      }
+        some: ['nesting', 'id'],
+      },
     }],
     'jsx-a11y/label-has-for': ['error', {
       required: {
-        some: ['nesting', 'id']
-      }
-    }]
-  }
+        some: ['nesting', 'id'],
+      },
+    }],
+  },
 };
