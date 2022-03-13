8ce6912550a27e3e000f13a9d23377e74c00a7c2
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-03-13 00:47:21 +0000

    added FB default auth files

---
+++ frontend/utils/apiKeys.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_APP_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_APP_STORAGE_BUCKET,
+    appId: process.env.NEXT_PUBLIC_APP_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_APP_MEASUREMENT_ID,
+  };
+  
+export default firebaseConfig;
