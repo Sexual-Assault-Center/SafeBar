6afce2a207598d273d9d5c64be3c2f97cf83bae1
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-04-25 12:42:12 -0500

    added GA and search event watch

--- frontend/pages/bars.js
+++ frontend/pages/bars.js
@@ -1,3 +1,28 @@
+/* eslint-disable quotes */
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import * as ga from '../utils/ga';
+
 export default function Bars() {
-  return <div>bars</div>;
+  const [query, setQuery] = useState('');
+
+  const search = () => {
+    ga.event({
+      action: "search",
+      params: {
+        search_term: query,
+      },
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={(event) => setQuery(event.target.value)} />
+      </div>
+      <div>
+        <Button onClick={() => search()}>Search</Button>
+      </div>
+    </div>
+  );
 }
