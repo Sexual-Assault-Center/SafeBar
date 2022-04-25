e8b232a5b87de3a725c7d65807f3121c2f3921b9
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-04-25 11:20:00 -0500

    setup base navigation components

---
+++ frontend/components/HeaderNav.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import {
+  Nav, Navbar, Container, Offcanvas, Button,
+} from 'react-bootstrap';
+import Link from 'next/link';
+
+export default function HeaderNav() {
+  // const [expanded, setExpanded] = useState(false);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Navbar as="nav" className="nb-fixed" expand={show}>
+      <Container fluid>
+        <Navbar.Brand href="/bars">
+          <h1 className="safespace">SafeBar</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" onClick={handleShow} />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+          show={show}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">
+              <h1 className="safespace">SafeBar</h1>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Link href="/resources" passHref>
+                <Button variant="link" onClick={handleClose}>Resources</Button>
+              </Link>
+              <Link href="/contacts" passHref>
+                <Button variant="link" onClick={handleClose}>Contacts</Button>
+              </Link>
+              <Link href="https://safebartn.org" passHref>
+                <Button variant="link">SafeBar website</Button>
+              </Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
+}
