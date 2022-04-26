1eece6816ca5445d8bb13691b7421f6d596cda9e
Trinity Christiana <trinitycterry@gmail.com>
2022-04-26 13:17:18 -0500

    Added bars

--- backend/h4tcsacapp/admin/__init__.py
+++ backend/h4tcsacapp/admin/__init__.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from h4tcsacapp.models.bars import Bar
 # Register your models here.
+
+
+class BarAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Bar, BarAdmin)
--- backend/h4tcsacapp/models/bars.py
+++ backend/h4tcsacapp/models/bars.py
@@ -1,7 +1,29 @@
-class Name(models.Model):
+from django.db import models
+
+
+class Bar(models.Model):
+    is_safebar = models.BooleanField("Is Safe Bar")
+    name = models.CharField("Name", max_length=1024,)
+    contact_name = models.CharField("Contact Name", max_length=1024,)
+    phone_number = models.IntegerField("Phone Number")
+    street_address = models.CharField("Street Address", max_length=1024,)
+    city = models.CharField(
+        "City",
+        max_length=1024,
+    )
+    zip_code = models.CharField(
+        "ZIP / Postal code",
+        max_length=12,
+    )
+    latitude = models.CharField(
+        "Latitude", max_length=10, blank=True, default="")
+    longitude = models.CharField(
+        "Longitude", max_length=10, blank=True, default="")
+    certification_date = models.DateField("Certification Date")
+    email = models.EmailField(("Email"), max_length=254)
+    image = models.URLField(("Image"), max_length=200)
+    description = models.CharField("Description", max_length=254)
+    website = models.URLField(("Website"), max_length=200)
 
     def __str__(self):
-        return 
-
-    def __unicode__(self):
-        return 
+        return self.name
