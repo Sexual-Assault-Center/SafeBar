334b788bb7f6496023ec1a375ffa9b398deacc8b
Trinity Christiana <trinitycterry@gmail.com>
2022-04-26 18:14:20 -0500

    Added bar report model

--- backend/h4tcsacapp/admin/__init__.py
+++ backend/h4tcsacapp/admin/__init__.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from h4tcsacapp.models.bar import Bar
+from h4tcsacapp.models.bar_report import BarReport
 from h4tcsacapp.models.rating import Rating
 from h4tcsacapp.models.report_type import ReportType
 
@@ -16,6 +17,11 @@ class ReportTypesAdmin(admin.ModelAdmin):
     pass
 
 
+class BarReportAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Bar, BarAdmin)
+admin.site.register(BarReport, BarReportAdmin)
 admin.site.register(Rating, RatingAdmin)
 admin.site.register(ReportType, ReportTypesAdmin)
--- backend/h4tcsacapp/models/bar_report.py
+++ backend/h4tcsacapp/models/bar_report.py
@@ -1,7 +1,10 @@
-# class Name(models.Model):
+from django.db import models
+from h4tcsacapp.models.bar import Bar
+from h4tcsacapp.models.report_type import ReportType
 
-#     def __str__(self):
-#         return
 
-#     def __unicode__(self):
-#         return
+class BarReport(models.Model):
+    bar_id = models.ForeignKey(Bar, on_delete=models.PROTECT)
+    report_type_id = models.ForeignKey(ReportType, on_delete=models.PROTECT)
+    comment = models.CharField(("Comment"), max_length=254)
+    uid = models.CharField(("User ID"), max_length=25)
