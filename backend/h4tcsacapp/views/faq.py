efece90363991b610eb221e5ee28dfba21c95d52
Trinity Christiana <trinitycterry@gmail.com>
2022-04-27 15:32:07 -0500

    Added faq, contacts, resources

---
+++ backend/h4tcsacapp/views/faq.py
@@ -0,0 +1,12 @@
+from h4tcsacapp.models.faq import FAQ
+from h4tcsacapp.serializers import FAQSerializer
+from rest_framework.viewsets import ModelViewSet
+
+
+class FAQViewSet(ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = FAQ.objects.all()
+    serializer_class = FAQSerializer
+    http_method_names = ['get']
