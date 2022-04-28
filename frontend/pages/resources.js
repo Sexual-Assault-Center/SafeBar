9ea953274d33dd6c08257130762aeb20cf71b304
Jesse Robinson <jesse0529robinson@gmail.com>
2022-04-28 17:41:46 +0000

    Removed comments

--- frontend/pages/resources.js
+++ frontend/pages/resources.js
@@ -3,16 +3,6 @@ import HeadDetails from '../components/HeadDetails';
 import resourcesData from '../resourcesData.json';
 import ResourcesCard from '../components/ResourcesCard';
 
-// const ResourceComponent = ({ ...props }) => (
-//   <>
-//     <h2>{props.title}</h2>
-//     <br />
-//     <p>{props.description}</p>
-//     <Button variant="primary" href={props.url} value="Link">Link</Button>
-//     <hr />
-//   </>
-// );
-
 export default function Resources() {
   return (
     <>
@@ -23,7 +13,6 @@ export default function Resources() {
           resourcesData.resources.map((resource) => <ResourcesCard {...resource} />)
         }
       </div>
-
     </>
   );
 }
