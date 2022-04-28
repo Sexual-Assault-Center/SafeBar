4f2d7109c358a8a661eef97610e8a01e7707f4bf
Jacob Chadwell <jtchadwell5@gmail.com>
2022-04-28 11:34:04 -0500

    [54] Submit bar

--- backend/h4tcsacrecertify/views.py
+++ backend/h4tcsacrecertify/views.py
@@ -8,22 +8,21 @@ def recertify_bar(request):
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = BarForm(request.POST)
-        form.Meta().model.is_safebar = False
         # check whether it's valid:
         if form.is_valid():
             new_bar = form.save()
             # redirect to a success page
             return HttpResponseRedirect('success/')
-        
-
-    # TODO: for next ticket
-    elif request.method == 'PATCH':
-        return ""
-    # if a GET (or any other method) we'll create a blank form
+        # We have validation on submit, but just in case, here's a path for errors
+        if form.is_valid() == False:
+            return HttpResponseRedirect('error/')
     else:
         form = BarForm()
 
     return render(request, 'submit.html', {'form': form})
 
 def success_redirect(request):
-    return render(request, 'redirect.html')
+    return render(request, 'success.html')
+
+def error_redirect(request):
+    return render(request, 'error.html')
