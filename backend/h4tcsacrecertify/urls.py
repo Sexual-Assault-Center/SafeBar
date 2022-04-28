0341be09bb5b913b7613e81547d4ec8da228258b
Jacob Chadwell <jtchadwell5@gmail.com>
2022-04-28 11:42:15 -0500

    add error to routes

--- backend/h4tcsacrecertify/urls.py
+++ backend/h4tcsacrecertify/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import recertify_bar, success_redirect
+from .views import recertify_bar, success_redirect, error_redirect
 
 urlpatterns = [
     path('', recertify_bar, name='submitBar'),
-    path('success/', success_redirect, name='success')
+    path('success/', success_redirect, name='success'),
+    path('error/', error_redirect, name='error')
 ]
