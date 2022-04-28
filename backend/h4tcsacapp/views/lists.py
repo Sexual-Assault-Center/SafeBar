c6590bd7974591315d4d48e37be6501d2d7d7ce2
Trinity Christiana <trinitycterry@gmail.com>
2022-04-28 18:50:47 -0500

    filtered by user

--- backend/h4tcsacapp/views/lists.py
+++ backend/h4tcsacapp/views/lists.py
@@ -5,47 +5,49 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
-class ListViewSet(ModelViewSet):
-  def create(self, request):
-    try:
-      list = List.objects.create(
-        uid = request.data['uid'],
-        name = request.data['name'] 
-        )
-      
-      serializer = ListSerializer(list)
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    except ValidationError as ex:
-      return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
-      
-  def update(self, request, pk=None):
-    try:
-      list = List.objects.get(pk=pk)
-      list.uid = request.data['uid']
-      list.name = request.data['name']
-      list.save()
-      return Response(None, status=status.HTTP_204_NO_CONTENT)
-    except ValidationError as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
-    except List.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-          
-  def list(self, request):
-    list = List.objects.all()
-   
-    serializer = ListSerializer(
-      list, many=True, context={'request': request}
-      )
-    return Response(serializer.data)
 
-  def destroy(self, request, pk=None):
-    try:
-      list = List.objects.get(pk=pk)
-      list.delete()
-      
-      return Response({}, status=status.HTTP_204_NO_CONTENT)
-    
-    except List.DoesNotExist as ex:
+class ListViewSet(ModelViewSet):
+    def create(self, request):
+        try:
+            list = List.objects.create(
+                uid=request.data['uid'],
+                name=request.data['name']
+            )
+
+            serializer = ListSerializer(list)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        try:
+            list = List.objects.get(pk=pk)
+            list.uid = request.data['uid']
+            list.name = request.data['name']
+            list.save()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        except ValidationError as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+        except List.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as ex:
+
+    def list(self, request):
+        uid = request.GET.get('uid', '')
+        list = List.objects.filter(uid=uid)
+
+        serializer = ListSerializer(
+            list, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        try:
+            list = List.objects.get(pk=pk)
+            list.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except List.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
