19a747472408eccb3a7e25dd9a91e20ede490bce
Katy Fry <katyvfry@gmail.com>
2022-04-28 16:32:46 +0000

    add accordion and footer

---
+++ frontend/stories/Accordion.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import AccordionComp from '../components/Accordion';
+
+export default {
+  title: 'Accordion',
+  component: AccordionComp,
+};
+
+export const Accordion = (args) => <AccordionComp {...args} />;
+
+Accordion.args = {
+    title: "title",
+    text: "text",
+}
+
+
+
+
