e3ac375aa6b2ae44db5cd6acbc3e4b6347d16504
Trinity Christiana <trinitycterry@gmail.com>
2022-04-28 14:21:11 -0500

    addd resources list

--- backend/h4tcsacadmin/views/resources.py
+++ backend/h4tcsacadmin/views/resources.py
@@ -4,21 +4,22 @@ from h4tcsacapp.serializers import ResourceSerializer
 
 
 class ResourceViews():
-    def run(self, request, action="list"):
+    def run(self, request, action="list", uuid=None):
         action_func = getattr(self, action)
-        return action_func(request)
+        return action_func(request, uuid)
 
-    def create(self, request):
-        pass
-
-    def list(self, request):
+    def create(self, request, uuid):
         resources = Resource.objects.all()
         data = ResourceSerializer(resources, many=True).data
-        print(data)
-        return render(request, "resources/list.html", {'resources': data})
+        return render(request, "admin/list.html", {'data': data, "headers": list(data[0].keys())})
 
-    def delete(self, request):
+    def list(self, request, uuid):
+        resources = Resource.objects.all()
+        data = ResourceSerializer(resources, many=True).data
+        return render(request, "admin/list.html", {'data': data, "headers": list(data[0].keys())})
+
+    def delete(self, request, uuid):
         pass
 
-    def update(self, request):
+    def update(self, request, uuid):
         pass
