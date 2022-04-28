a64be8e2db4b6253f787be33621dfb9d26060565
Madden Purcell <maddenpurcell@gmail.com>
2022-04-28 20:57:02 +0000

    Added Accordion Component to help.js

--- frontend/pages/help.js
+++ frontend/pages/help.js
@@ -1,5 +1,6 @@
 import HeadDetails from '../components/HeadDetails';
 import { userHelp, barHelp } from '../help.json';
+import AccordionComp from '../components/Accordion';
 
 export default function Help() {
   return (
@@ -7,8 +8,22 @@ export default function Help() {
       <HeadDetails title="Get Help" description="Making Nightlife Safer for Everyone" />
       <h1>Help</h1>
       <div id="helpDiv">
-        <h5>{userHelp.title}</h5>
-        <h5>{barHelp.title}</h5>
+        <AccordionComp title={userHelp.title}>
+          <div id="userDiv">
+            <p>{userHelp.emergency}</p>
+            <p>{userHelp.hotline}</p>
+            <a href={userHelp.chatUrl}>{userHelp.chat}</a>
+          </div>
+        </AccordionComp>
+        <AccordionComp title={barHelp.title}>
+          <div id="barDiv">
+            <p>{barHelp.direct}</p>
+            <p>{barHelp.distract}</p>
+            <p>{barHelp.delegate}</p>
+            <p>{barHelp.delay}</p>
+            <p>{barHelp.document}</p>
+          </div>
+        </AccordionComp>
       </div>
     </>
   );
