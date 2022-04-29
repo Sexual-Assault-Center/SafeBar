9b9d1ff74788c34d749e6a6034a620267c3cf2d7
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-04-29 12:11:38 -0500

    added bars

--- frontend/pages/bars.js
+++ frontend/pages/bars.js
@@ -1,7 +1,6 @@
 /* eslint-disable consistent-return */
 import { useState, useEffect } from 'react';
 // import * as ga from '../utils/ga';
-import barsData from '../barsData.json';
 // import Searchbar from '../components/Searchbar';
 import BarCard from '../components/BarCard';
 import HeadDetails from '../components/HeadDetails';
@@ -20,9 +19,11 @@ export default function Bars() {
   const { user } = useAuth();
 
   useEffect(() => {
-    getAllBars().then(console.log);
-    setSafebars(barsData.filter((bar) => bar.safebar));
-    setBars(barsData.filter((bar) => !bar.safebar));
+    getAllBars().then((barData) => {
+      console.log(barData);
+      setSafebars(barData.filter((bar) => bar.safebar));
+      setBars(barData.filter((bar) => !bar.safebar));
+    });
   }, []);
 
   // const search = () => {
@@ -54,10 +55,10 @@ export default function Bars() {
       {/* <Searchbar onClick={() => search()} onChange={(e) => handleChange(e)} value={value}>Search</Searchbar> */}
       <div className="card-cont d-flex flex-wrap">
         {
-          safebars.map((bar) => <BarCard key={bar.id} {...bar} user={user} func={checkUserStatus} />)
+          safebars.map((bar) => <BarCard key={bar.uuid} {...bar} user={user} func={checkUserStatus} />)
         }
         {
-          bars.map((bar) => <BarCard key={bar.id} {...bar} user={user} func={checkUserStatus} />)
+          bars.map((bar) => <BarCard key={bar.uuid} {...bar} user={user} func={checkUserStatus} />)
         }
       </div>
     </>
