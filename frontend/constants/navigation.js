b061e473b5b87d11d130b5613726351f2feaf22f
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-04-29 01:51:48 -0500

    styled footer

--- frontend/constants/navigation.js
+++ frontend/constants/navigation.js
@@ -1,6 +1,24 @@
 const NAVIGATION = {
-  FOOTER: ['Home', 'Explore', 'HELP', 'Lists', 'Login'],
-  TRAINING_LINK: 'https://sacenter.org/resources/safe-bar-training-program.aspx',
+  FOOTER: [
+    {
+      path: '/',
+      text: 'Home',
+    },
+    {
+      path: 'bars',
+      text: 'Explore',
+    },
+    {
+      path: 'help',
+      text: 'HELP',
+    },
+    {
+      path: 'lists',
+      text: 'Lists',
+    },
+  ],
+  TRAINING_LINK:
+    'https://sacenter.org/resources/safe-bar-training-program.aspx',
 };
 
 export default NAVIGATION;
