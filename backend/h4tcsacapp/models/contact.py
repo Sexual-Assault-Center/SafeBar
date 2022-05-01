ca145204f980b8bca385d9b051175734a68d3ac7
Trinity Christiana <trinitycterry@gmail.com>
2022-05-01 15:46:05 -0500

    Updated model rep names for form labels

--- backend/h4tcsacapp/models/contact.py
+++ backend/h4tcsacapp/models/contact.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Contact(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(("Sponser Name"), max_length=50)
+    name = models.CharField(("Sponser"), max_length=50)
     email = models.EmailField(("Email"), max_length=254)
     phone_number = models.BigIntegerField("Phone Number")
     contact_name = models.CharField(("Contact Name"), max_length=50)
