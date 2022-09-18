8bca0133c6e18c13cbafe3dc0828f0fbcc72f7b1
michaelbentley95 <44556637+michaelbentley95@users.noreply.github.com>
2022-09-17 22:29:46 -0500

    Formatting changes to homepage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,46 +1,17 @@
-// import { useEffect, useState } from 'react';
-// import ReactPlayer from 'react-player/lazy';
-// import Link from 'react-router-dom';
-// import Image from 'next/image';
-import {
-  BsTwitter, BsFacebook, BsYoutube, BsInstagram,
-} from 'react-icons/bs';
+/* eslint-disable object-curly-newline */
+import { BsTwitter, BsFacebook, BsYoutube, BsInstagram } from 'react-icons/bs';
 import HeadDetails from '../components/HeadDetails';
 import { cardData } from '../data/homepageCards';
 import FeatureCard from '../components/FeatureCard';
-// import { getRequest } from '../utils/api';
 
 export default function Home() {
-  // const [landing, setLanding] = useState({});
-
-  // useEffect(() => {
-  //   let isMounted = true;
-  //   getRequest('landing).then((landingObj) => {
-  //     if (isMounted) {
-  //       setLanding(landingObj);
-  //     }
-  //   });
-  //   return () => {
-  //     isMounted = false;
-  //   };
-  // }, []);
-
   return (
     <div>
       <HeadDetails
         title="SAFEBAR"
         description="Making Nightlife Safer for Everyone"
       />
-      <h2>GET INVOLVED</h2>
-      {/* <div mediaUrl={landing.mediaUrl} isVideo={landing.isVideo} clickUrl="foobar">
-        {landing.isVideo
-          ? <ReactPlayer url={landing.mediaUrl} />
-          : (
-            <Link to="clickUrl">
-              <Image src={landing.mediaUrl} alt={landing.title} />
-            </Link>
-          )}
-      </div> */}
+      <h2 className="my-0">GET INVOLVED</h2>
       <div className="card-cont d-flex flex-wrap justify-content-center align-items-center">
         {cardData.map((card) => (
           <FeatureCard
@@ -49,22 +20,41 @@ export default function Home() {
             buttonText={card.buttonText}
             img={card.image}
             text={card.text}
-            onClick={(e) => { e.preventDefault(); window.open(card.href, 'target'); }}
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(card.href, 'target');
+            }}
             disabledButton={card.disabledButton}
           />
         ))}
       </div>
       <div className="social-banner d-flex flex-row flex-wrap align-items-center justify-content-evenly space-between">
-        <a href="https://twitter.com/NashvilleSAC" target="_blank" rel="noreferrer">
+        <a
+          href="https://twitter.com/NashvilleSAC"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsTwitter className="footerIcon text-white m-2" size={30} />
         </a>
-        <a href="https://www.facebook.com/nashvilleSAC" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.facebook.com/nashvilleSAC"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsFacebook className="footerIcon text-white m-2" size={30} />
         </a>
-        <a href="https://www.youtube.com/channel/UCOBCjNfljUuBR8h1I7f531A" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.youtube.com/channel/UCOBCjNfljUuBR8h1I7f531A"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsYoutube className="footerIcon text-white m-2" size={30} />
         </a>
-        <a href="https://www.instagram.com/nashvillesac/" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.instagram.com/nashvillesac/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <BsInstagram className="footerIcon text-white m-2" size={30} />
         </a>
       </div>
